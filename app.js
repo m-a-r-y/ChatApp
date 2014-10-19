b409515c428986871e6b93abc8caa76f16e276d3
Mary Le <maaaryle@gmail.com>
2014-10-19 10:55:19 -0700

    add stylesheet

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+  });
+});
+
+http.listen(9900, function(){
+  console.log('over 9000?!?!');
+});
